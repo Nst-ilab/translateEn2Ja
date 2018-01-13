76580cc86d59a072a4ccea08e70fa809a8c35879
makoto <makoto_okamura@nri-st.co.jp>
2018-01-13 14:02:28 +0000

    fix lambda function
    fix lambda function
    
     Author:    makoto <makoto_okamura@nri-st.co.jp>

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # translateEn2Ja
+
+
+    data={}
+    data['Dic'] = 'EJDict'
+    data['Word'] = searchWord
+    data['Scope'] = 'HEADWORD'
+    data['Match'] = 'EXACT'
+    data['Merge'] = 'OR'
+    data['Prof'] = 'XHTML'
+    data['PageSize'] = '10'
+    data['PageIndex'] = '0'
+    url_values = parse.urlencode(data)
+    url = 'http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite'
+    full_url = url + '?' + url_values
+    data = request.urlopen(full_url)
+
+    tree = ET.parse(data)
+    root = tree.getroot()
+    element = root.find()
--- translateEn2Ja/lambda_function.py
+++ translateEn2Ja/lambda_function.py
@@ -1,19 +1,61 @@
 # coding: UTF-8
+import boto3
 import urllib.request
 import xml.etree.ElementTree as ET
 
+#Region指定しないと、デフォルトのUSリージョンが使われる
+client_lambda = boto3.client('lambda', region_name='ap-northeast-1')
 
 def lambda_handler(event, context):
-    # TODO implement
-    
-    # 英語のみの発言に反応する
-    
-    # 検索ワードを格納
-    # searchWord = s
-    
-    # 検索した見出し文字を取得
 
-    # 見出し文字から翻訳結果を取得
+    # このサービスが動作するか決定する。英単語１つしか入ってないときに動かす
+    line_text = event["lineMessage"]["events"][0]["message"]["text"]
+    first_content = event["analysedMessage"]["tokens"][0]["text"]["content"]
+    language = event["analysedMessage"]["language"]
+    if line_text == first_content and language == "en":
+        search_word = line_text
+    else:
+        return None
     
-    # 検索結果を回答
-    return ''
+    # 検索
+    item_id = get_item_id(search_word)
+    translated_text = get_translated_text(item_id)
+    res_translated_text = split_translated_text(translated_text, '\t')
+
+    #取得した翻訳結果を返す
+    if res_translated_text is None:
+        return None
+    else:
+        return { "message" : res_translated_text }
+
+# 翻訳サービスが返すxmlから結果を抽出する
+def get_xml_element_text(url, tag):
+    try:
+        xml = urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+        print('error code : ' + str(e.code))
+        print('error read : ' + str(e.read()))
+        return ''
+    tree = ET.parse(xml)
+    root = tree.getroot()
+    element = root.find('.//{http://btonic.est.co.jp/NetDic/NetDicV09}' + tag)
+    text = element.text
+    return text
+
+# 翻訳サービスからワードのIDを取得
+def get_item_id(search_word):
+    head = 'http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite?Dic=EJdict&Word='
+    end = '&Scope=HEADWORD&Match=EXACT&Merge=OR&Prof=XHTML&PageSize=20&PageIndex=0'
+    url = head + search_word + end
+    return get_xml_element_text(url, 'ItemID')
+
+# 翻訳サービスにワードIDを投げて結果を取得
+def get_translated_text(item_id):
+    head = 'http://public.dejizo.jp/NetDicV09.asmx/GetDicItemLite?Dic=EJdict&Item='
+    end = '&Loc=&Prof=XHTML'
+    url = head + item_id + end
+    return get_xml_element_text(url, 'Body/div/div')
+
+# 翻訳テキストの整形
+def split_translated_text(translated_text, split_word):
+    return translated_text.split(split_word)
