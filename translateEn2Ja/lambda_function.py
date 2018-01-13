30a43f3fb927f28cefea2d0b074f60be121b332c
EC2 Default User <ec2-user@ip-10-0-0-12.ap-southeast-1.compute.internal>
2018-01-13 12:38:54 +0000

    coding get_translated_text

--- translateEn2Ja/lambda_function.py
+++ translateEn2Ja/lambda_function.py
@@ -1,3 +1,19 @@
+# coding: UTF-8
+import urllib.request
+import xml.etree.ElementTree as ET
+
+
 def lambda_handler(event, context):
     # TODO implement
+    
+    # 英語のみの発言に反応する
+    
+    # 検索ワードを格納
+    # searchWord = s
+    
+    # 検索した見出し文字を取得
+
+    # 見出し文字から翻訳結果を取得
+    
+    # 検索結果を回答
     return ''
